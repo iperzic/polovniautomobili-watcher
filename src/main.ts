3b8e50f45281e6ccfe0a9bde7e16839035ddd31e
Igor Perzic <iperzic@yahoo.com>
2024-01-29 22:55:02 +0100

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import { PlaywrightCrawler, log } from 'crawlee';
+import { router } from './router.js';
+
+// This is better set with CRAWLEE_LOG_LEVEL env var
+// or a configuration option. This is just for show 😈
+log.setLevel(log.LEVELS.DEBUG);
+
+log.debug('Setting up crawler.');
+const crawler = new PlaywrightCrawler({
+    // Instead of the long requestHandler with
+    // if clauses we provide a router instance.
+    requestHandler: router,
+});
+
+await crawler.run(['https://warehouse-theme-metal.myshopify.com/collections']);
