6ffbdb16ad7973792dc08a7cfa08399334af443e
Igor Perzic <iperzic@yahoo.com>
2024-01-30 09:47:24 +0100

    add initial polovniautomobili.com selectors

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,12 @@
 import { PlaywrightCrawler, log } from 'crawlee';
 import { router } from './router.js';
 
-// This is better set with CRAWLEE_LOG_LEVEL env var
-// or a configuration option. This is just for show 😈
 log.setLevel(log.LEVELS.DEBUG);
 
 log.debug('Setting up crawler.');
 const crawler = new PlaywrightCrawler({
-    // Instead of the long requestHandler with
-    // if clauses we provide a router instance.
     requestHandler: router,
+    persistCookiesPerSession: false
 });
 
-await crawler.run(['https://warehouse-theme-metal.myshopify.com/collections']);
+await crawler.run(['https://www.polovniautomobili.com/auto-oglasi/pretraga?brand=ford&model%5B%5D=b-max&brand2=&price_from=&price_to=&year_from=&year_to=&flywheel=&atest=&door_num=&submit_1=&without_price=1&date_limit=&showOldNew=all&modeltxt=&engine_volume_from=&engine_volume_to=&power_from=&power_to=&mileage_from=&mileage_to=&emission_class=&seat_num=&wheel_side=&registration=&country=&country_origin=&city=&registration_price=&page=&sort=']);
--- src/router.ts
+++ src/router.ts
@@ -1,84 +1,65 @@
 import { createPlaywrightRouter, Dataset } from 'crawlee';
 
-// createPlaywrightRouter() is only a helper to get better
-// intellisense and typings. You can use Router.create() too.
 export const router = createPlaywrightRouter();
 
-// This replaces the request.label === DETAIL branch of the if clause.
 router.addHandler('DETAIL', async ({ request, page, log }) => {
     log.debug(`Extracting data: ${request.url}`);
-    const urlPart = request.url.split('/').slice(-1); // ['sennheiser-mke-440-professional-stereo-shotgun-microphone-mke-440']
-    const manufacturer = urlPart[0].split('-')[0]; // 'sennheiser'
 
-    const title = await page.locator('.product-meta h1').textContent();
-    const sku = await page
-        .locator('span.product-meta__sku-number')
-        .textContent();
+    const generalInfoBox = page.locator('#classified-content > div.uk-width-1-1 > section > section > div > div > div > div > div > div')
+    const rightPanel = generalInfoBox.locator('div.uk-width-medium-1-2')
+    const leftPanel = generalInfoBox.locator('div.uk-width-large-1-2.uk-width-medium-1-1.uk-width-1-1')
 
-    const priceElement = page
-        .locator('span.price')
-        .filter({
-            hasText: '$',
-        })
-        .first();
+    const condition = await leftPanel.locator('div:nth-child(1) > div > div.uk-width-1-2.uk-text-bold').textContent()
+    const make = await leftPanel.locator('div:nth-child(2) > div > div.uk-width-1-2.uk-text-bold').textContent()
+    const model = await leftPanel.locator('div:nth-child(3) > div > div.uk-width-1-2.uk-text-bold').textContent()
+    const year = await leftPanel.locator('div:nth-child(4) > div > div.uk-width-1-2.uk-text-bold').textContent()
+    const kms = await leftPanel.locator('div:nth-child(5) > div > div.uk-width-1-2.uk-text-bold').textContent()
+    const bodyType = await leftPanel.locator('div:nth-child(6) > div > div.uk-width-1-2.uk-text-bold').textContent()
+    const fuel = await leftPanel.locator('div:nth-child(7) > div > div.uk-width-1-2.uk-text-bold').textContent()
 
-    const currentPriceString = (await priceElement.textContent()) ?? '';
-    const rawPrice = currentPriceString.split('$')[1];
-    const price = Number(rawPrice.replaceAll(',', ''));
+    const cc = await rightPanel.locator('div:nth-child(1) > div > div.uk-width-1-2.uk-text-bold').textContent()
+    const engine = await rightPanel.locator('div:nth-child(2) > div > div.uk-width-1-2.uk-text-bold').textContent()
+    const fixedPrice = await rightPanel.locator('div:nth-child(3) > div > div.uk-width-1-2.uk-text-bold').textContent()
+    const swap = await rightPanel.locator('div:nth-child(4) > div > div.uk-width-1-2.uk-text-bold').textContent()
+    const id = await rightPanel.locator('div:nth-child(5) > div > div.uk-width-1-2.uk-text-bold').textContent();
 
-    const inStockElement = page
-        .locator('span.product-form__inventory')
-        .filter({
-            hasText: 'In stock',
-        })
-        .first();
+    const description = await page.locator('#classifiedReplaceDescription > div').innerText()
 
-    const inStock = (await inStockElement.count()) > 0;
 
     const results = {
         url: request.url,
-        manufacturer,
-        title,
-        sku,
-        currentPrice: price,
-        availableInStock: inStock,
+        condition,
+        make,
+        model,
+        year,
+        kms,
+        bodyType,
+        fuel,
+        cc,
+        engine,
+        fixedPrice,
+        swap,
+        id,
+        description
     };
 
     log.debug(`Saving data: ${request.url}`);
     await Dataset.pushData(results);
 });
 
-router.addHandler('CATEGORY', async ({ page, enqueueLinks, request, log }) => {
+router.addDefaultHandler(async ({ request, page, enqueueLinks, log }) => {
     log.debug(`Enqueueing pagination for: ${request.url}`);
-    // We are now on a category page. We can use this to paginate through and enqueue all products,
-    // as well as any subsequent pages we find
 
-    await page.waitForSelector('.product-item > a');
+    await page.waitForSelector('#search-results  article.classified.ordinaryClassified > div.textContentHolder > div.textContent a');
     await enqueueLinks({
-        selector: '.product-item > a',
-        label: 'DETAIL', // <= note the different label
+        selector: '#search-results  article.classified.ordinaryClassified > div.textContentHolder > div.textContent a',
+        label: 'DETAIL',
     });
 
-    // Now we need to find the "Next" button and enqueue the next page of results (if it exists)
-    const nextButton = await page.$('a.pagination__next');
+    const nextButton = await page.$('a.js-pagination-next');
     if (nextButton) {
         await enqueueLinks({
-            selector: 'a.pagination__next',
-            label: 'CATEGORY', // <= note the same label
+            selector: 'a.js-pagination-next',
         });
     }
-});
-
-// This is a fallback route which will handle the start URL
-// as well as the LIST labeled URLs.
-router.addDefaultHandler(async ({ request, page, enqueueLinks, log }) => {
-    log.debug(`Enqueueing categories from page: ${request.url}`);
-    // This means we're on the start page, with no label.
-    // On this page, we just want to enqueue all the category pages.
-
-    await page.waitForSelector('.collection-block-item');
-    await enqueueLinks({
-        selector: '.collection-block-item',
-        label: 'CATEGORY',
-    });
 });
