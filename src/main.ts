8a33d3692deda3c1a68a87ba1c0c09613f1400b0
Igor Perzic <iperzic@yahoo.com>
2024-02-02 10:06:24 +0100

    export dataset to JSON

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { log, CheerioCrawler } from 'crawlee';
+import { log, CheerioCrawler, Dataset } from 'crawlee';
 import { router } from './router.js';
 
 log.setLevel(log.LEVELS.DEBUG);
@@ -10,3 +10,5 @@ const crawler = new CheerioCrawler({
 });
 
 await crawler.run(['https://www.polovniautomobili.com/auto-oglasi/pretraga?brand=ford&model%5B%5D=b-max&brand2=&price_from=&price_to=&year_from=&year_to=&flywheel=&atest=&door_num=&submit_1=&without_price=1&date_limit=&showOldNew=all&modeltxt=&engine_volume_from=&engine_volume_to=&power_from=&power_to=&mileage_from=&mileage_to=&emission_class=&seat_num=&wheel_side=&registration=&country=&country_origin=&city=&registration_price=&page=&sort=']);
+
+await Dataset.exportToJSON(`${Date.now()}`, { toKVS: 'output' })
