fde0ec4425c521cf54e1a98869597564b51d3e70
Igor Perzic <iperzic@yahoo.com>
2024-02-02 10:22:22 +0100

    refactor files

---
+++ src/crawler/index.ts
@@ -0,0 +1,17 @@
+import { log, CheerioCrawler, Dataset } from 'crawlee';
+import { router } from './router.js';
+
+export default async function crawl() {
+    log.setLevel(log.LEVELS.DEBUG);
+
+    log.debug('Setting up crawler.');
+    const crawler = new CheerioCrawler({
+        requestHandler: router,
+        persistCookiesPerSession: false
+    });
+
+    await crawler.run(['https://www.polovniautomobili.com/auto-oglasi/pretraga?brand=ford&model%5B%5D=b-max&brand2=&price_from=&price_to=&year_from=&year_to=&flywheel=&atest=&door_num=&submit_1=&without_price=1&date_limit=&showOldNew=all&modeltxt=&engine_volume_from=&engine_volume_to=&power_from=&power_to=&mileage_from=&mileage_to=&emission_class=&seat_num=&wheel_side=&registration=&country=&country_origin=&city=&registration_price=&page=&sort=']);
+
+    await Dataset.exportToJSON(`${Date.now()}`, { toKVS: 'output' })
+}
+
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,3 @@
-import { log, CheerioCrawler, Dataset } from 'crawlee';
-import { router } from './router.js';
+import crawl from "./crawler/index.js";
 
-log.setLevel(log.LEVELS.DEBUG);
-
-log.debug('Setting up crawler.');
-const crawler = new CheerioCrawler({
-    requestHandler: router,
-    persistCookiesPerSession: false
-});
-
-await crawler.run(['https://www.polovniautomobili.com/auto-oglasi/pretraga?brand=ford&model%5B%5D=b-max&brand2=&price_from=&price_to=&year_from=&year_to=&flywheel=&atest=&door_num=&submit_1=&without_price=1&date_limit=&showOldNew=all&modeltxt=&engine_volume_from=&engine_volume_to=&power_from=&power_to=&mileage_from=&mileage_to=&emission_class=&seat_num=&wheel_side=&registration=&country=&country_origin=&city=&registration_price=&page=&sort=']);
-
-await Dataset.exportToJSON(`${Date.now()}`, { toKVS: 'output' })
+await crawl();
